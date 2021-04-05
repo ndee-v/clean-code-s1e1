77ec1cdd23a01d705d85c0f2f41ce7681ef7e153
ndee-v <dronez86@gmail.com>
2021-04-05 21:43:54 +0600

    refactor: rule 3.4 add class for tag li

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var completedTasksHolder=document.getElementById("completed-tasks");//completed-
 var createNewTaskElement=function(taskString){
 
     var listItem=document.createElement("li");
-
+    listItem.className="item";
     //input (checkbox)
     var checkBox=document.createElement("input");//checkbx
     //label
